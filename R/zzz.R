936b091aed78cf2a3eae6222e8fd1d50084fca4b
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-03-04 17:49:21 +0000

    replace deprecated .find.packages() with find.packages()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/hapFabia@73893 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -12,7 +12,7 @@
         }
         else {
             vigMtrx = readRDS(vigFile)
-            vigs = file.path(chartr("\\", "/", .find.package("hapFabia")), "doc", vigMtrx[,
+            vigs = file.path(chartr("\\", "/", find.package("hapFabia")), "doc", vigMtrx[,
                 "PDF"])
             names(vigs) = vigMtrx[, "Title"]
             if (!"Vignettes" %in% winMenuNames())
